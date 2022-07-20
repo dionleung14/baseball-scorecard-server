f10bca343efa8a81b7f0e00e7554b776d87ffcf6
Dion L <dioncleung@gmail.com>
2022-07-19 21:58:22 -0700

    first commit

---
+++ src/main/java/com/dcl/baseball/baseball/BaseballServerApplication.java
@@ -0,0 +1,13 @@
+package com.dcl.baseball.baseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcl/baseball/baseball/BaseballServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcl.baseball.baseball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
