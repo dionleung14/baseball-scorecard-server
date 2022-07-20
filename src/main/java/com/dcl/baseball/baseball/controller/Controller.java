46658adb1575c60f22fde17f70a676c93f2b679c
Dion L <dioncleung@gmail.com>
2022-07-19 22:20:59 -0700

    testing out hidden variables with application.properties

--- src/main/java/com/dcl/baseball/baseball/controller/Controller.java
+++ src/main/java/com/dcl/baseball/baseball/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.dcl.baseball.baseball.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "api/v1/game")
 public class Controller {
+  private String baseUrl = "http://api.sportradar.us/mlb/trial/v7/en/games/";
+
+  @Value("${api.key}")
+  private String apiKey;
 
   @GetMapping("/home")
   public void testRoute() {
     System.out.println("We are in business");
+    // System.out.println(apiKey);
+    System.out.println(baseUrl + "2022/07/16/boxscore.json?api_key=" + apiKey);
   }
 }
